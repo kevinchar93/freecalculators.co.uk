1a6dabc2e4b6e274e943a45b4eb53bc767d06209
kevinchar93 <kevincharles018@gmail.com>
2026-07-19 11:30:48 +0100

    feat: nest calculators correctly

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
 
-Route::inertia('/mortgage-calculator', 'MortgageCalculatorPage')->name('mortgage-calculator');
+Route::prefix('calculators')->group(function () {
+    Route::inertia('/mortgage', 'MortgageCalculatorPage')->name('mortgage-calculator');
+});
