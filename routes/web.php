4e171f57cabb4b4f48b8630d1e8fe8fafcd2ce5d
kevinchar93 <kevincharles018@gmail.com>
2026-07-19 11:35:36 +0100

    feat: rename page

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::inertia('/', 'welcome')->name('home');
 
 Route::prefix('calculators')->group(function () {
-    Route::inertia('/mortgage', 'MortgageCalculatorPage')->name('mortgage-calculator');
+    Route::inertia('/mortgage', 'mortgage-calculator-page')->name('mortgage-calculator');
 });
