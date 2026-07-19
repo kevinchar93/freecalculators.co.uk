7bda04c50ed87ffac90d07ae6316de29c0a58bef
kevinchar93 <kevincharles018@gmail.com>
2026-07-19 12:10:17 +0100

    feat: add fonts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
+import { local } from 'laravel-vite-plugin/fonts';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -12,8 +12,39 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
             refresh: true,
             fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
+                local('DM Sans', {
+                    alias: 'sans',
+                    variable: '--font-sans',
+                    optimizedFallbacks: false,
+                    variants: [
+                        {
+                            src: 'resources/css/fonts/DMSans-VariableFont_opsz,wght.woff2',
+                            weight: '100 1000',
+                            style: 'normal',
+                        },
+                        {
+                            src: 'resources/css/fonts/DMSans-Italic-VariableFont_opsz,wght.woff2',
+                            weight: '100 1000',
+                            style: 'italic',
+                        },
+                    ],
+                }),
+                local('Inter', {
+                    alias: 'logo',
+                    variable: '--font-logo',
+                    optimizedFallbacks: false,
+                    variants: [
+                        {
+                            src: 'resources/css/fonts/Inter-VariableFont_opsz,wght.woff2',
+                            weight: '100 900',
+                            style: 'normal',
+                        },
+                        {
+                            src: 'resources/css/fonts/Inter-Italic-VariableFont_opsz,wght.woff2',
+                            weight: '100 900',
+                            style: 'italic',
+                        },
+                    ],
                 }),
             ],
         }),
