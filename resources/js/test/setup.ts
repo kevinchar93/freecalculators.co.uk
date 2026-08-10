26aaea207505b13d480cd78135b400d0251038cd
kevinchar93 <kevincharles018@gmail.com>
2026-08-11 00:29:56 +0100

    changes

--- resources/js/test/setup.ts
+++ resources/js/test/setup.ts
@@ -1,2 +1,11 @@
 import '@testing-library/jest-dom/vitest';
 import './mocks/inertia';
+
+// jsdom doesn't implement ResizeObserver, which Radix primitives (e.g. Checkbox) use.
+class ResizeObserverStub {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+globalThis.ResizeObserver ??= ResizeObserverStub;
