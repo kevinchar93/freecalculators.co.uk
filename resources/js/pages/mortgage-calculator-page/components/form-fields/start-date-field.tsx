cb7b5e2a575f8284d24d0fed0049e593e51dc08b
kevinchar93 <kevincharles018@gmail.com>
2026-08-30 09:19:21 +0100

    review StartDateField

--- resources/js/pages/mortgage-calculator-page/components/form-fields/start-date-field.tsx
+++ resources/js/pages/mortgage-calculator-page/components/form-fields/start-date-field.tsx
@@ -6,6 +6,10 @@ import copy from '../../copy.json';
 import { useMortgageStore } from '../../store';
 import { inputClass } from './field-styles';
 
+const now = new Date();
+const MIN_MONTH = '1970-01';
+const MAX_MONTH = `${now.getFullYear() + 5}-12`; // 5 years in future
+
 export function StartDateField() {
   const fieldId = useId();
   const startDate = useMortgageStore((s) => s.startDate);
@@ -18,8 +22,18 @@ export function StartDateField() {
         type="month"
         id={fieldId}
         name="startDate"
+        min={MIN_MONTH}
+        max={MAX_MONTH}
         value={startDate}
         onChange={(event) => setStartDate(event.target.value)}
+        onBlur={(event) => {
+          const value = event.target.value;
+          if (value && value < MIN_MONTH) {
+            setStartDate(MIN_MONTH);
+          } else if (value && value > MAX_MONTH) {
+            setStartDate(MAX_MONTH);
+          }
+        }}
         className={cn('mt-2', inputClass)}
       />
     </div>
